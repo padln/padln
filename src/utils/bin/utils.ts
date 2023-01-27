e3452fda50ec6c32b3ae02c54ba37cfb28a1fbe3
padln <81055477+padln@users.noreply.github.com>
2023-01-27 08:49:08 -0800

    Update utils.ts

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -77,10 +77,5 @@ export const banner = (args?: string[]): string => {
 Type 'help' to see list of available commands.
 
 --
-The project is open-source 🎉 type 'repo' to check out the repository.
-
-New 🎉: Try out the new 'theme' command. See all available themes <a href="https://github.com/m4tt72/terminal/tree/master/docs/themes">in the docs</a>.
-New 🎉: New command 'neofetch', for you linux.
---
 `;
 };
